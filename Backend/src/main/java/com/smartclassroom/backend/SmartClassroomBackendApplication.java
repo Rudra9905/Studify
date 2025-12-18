845499c685a3ce4a4899106794bfd819f2fd6806
Rudra mehta <r.v.mehta9905@gmail.com>
2025-12-18 23:19:24 +0530

    third

--- Backend/src/main/java/com/smartclassroom/backend/SmartClassroomBackendApplication.java
+++ Backend/src/main/java/com/smartclassroom/backend/SmartClassroomBackendApplication.java
@@ -2,10 +2,8 @@ package com.smartclassroom.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-@PropertySource("classpath:.env")
 public class SmartClassroomBackendApplication {
        public static void main(String[] args) {
         SpringApplication.run(SmartClassroomBackendApplication.class, args);
